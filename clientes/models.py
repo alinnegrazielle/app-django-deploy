475a742d2825c15f46e8278a2489b620ae87f49b
alinne <alinnegraziellecontato@gmail.com>
2021-06-13 15:46:17 -0300

    created superuser

--- clientes/models.py
+++ clientes/models.py
@@ -7,3 +7,6 @@ class Person(models.Model):
     age = models.IntegerField()
     salary = models.DecimalField(max_digits=5, decimal_places=2)
     bio = models.TextField()
+
+    def __str__(self):
+        return self.first_name
