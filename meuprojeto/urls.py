1d2a8fea1d8f3380dc40551b1868bb2f6601d684
alinne <alinnegraziellecontato@gmail.com>
2021-06-15 11:49:49 -0300

    Viewing media files

--- meuprojeto/urls.py
+++ meuprojeto/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import hello
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', hello),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
