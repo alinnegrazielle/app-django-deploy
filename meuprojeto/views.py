1ec75204872229b20164455db26ccf0e3be37c94
alinne <alinnegraziellecontato@gmail.com>
2021-06-16 19:28:29 -0300

    criado projeto final

--- meuprojeto/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-
-
-def hello(request):
-    return render(request, 'index.html')
--- projeto final/gestaoClientes/gestaoClientes/urls.py
+++ projeto final/gestaoClientes/gestaoClientes/urls.py
@@ -1,7 +1,7 @@
-"""meuprojeto URL Configuration
+"""gestaoClientes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hello
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', hello),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
