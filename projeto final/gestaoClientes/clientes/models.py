aac9f61daef53d546ee4b67702290b67be41fe16
alinne <alinnegraziellecontato@gmail.com>
2021-06-23 21:12:19 -0300

    created models

---
+++ projeto final/gestaoClientes/clientes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    salary = models.DecimalField(max_digits=5, decimal_places=2)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
