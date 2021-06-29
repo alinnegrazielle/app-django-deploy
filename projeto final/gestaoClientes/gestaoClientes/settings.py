d8f2a4deaeabf6146a116ddab6cf896ceda214d8
alinne <alinnegraziellecontato@gmail.com>
2021-06-28 21:50:04 -0300

    adaptando projeto par Keroku

--- projeto final/gestaoClientes/gestaoClientes/settings.py
+++ projeto final/gestaoClientes/gestaoClientes/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!xd5gwdzs6*68shw0b3ucv#b$@!w19h&t1=lwyf80(6%=&mblp'
-
+SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =
 
 ALLOWED_HOSTS = []
 
